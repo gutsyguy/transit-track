93b77a2901124968bef60031d4c49d388fd63a59
dav <usr21p1@gmail.com>
2024-01-20 08:59:55 -0800

    Map with bus points

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,35 @@
-import MapView from "react-native-maps";
-import { View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 import { styles } from "../App";
 
-export default function MapSection() {
+export type LatLong = [number, number];
+
+interface MapSectionProps {
+  bus_positions: LatLong[];
+  camera_position: LatLong;
+  camera_size: LatLong;
+}
+
+export default function MapSection({
+  bus_positions,
+  camera_position,
+  camera_size,
+}: MapSectionProps) {
+  const markers = bus_positions.map((point) => {
+    return <Marker coordinate={{ latitude: point[0], longitude: point[1] }} />;
+  });
+
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} provider="google" />
-    </View>
+    <MapView
+      style={styles.map}
+      region={{
+        latitude: camera_position[0],
+        longitude: camera_position[1],
+        latitudeDelta: camera_size[0],
+        longitudeDelta: camera_size[1],
+      }}
+      provider="google"
+    >
+      {markers}
+    </MapView>
   );
 }
