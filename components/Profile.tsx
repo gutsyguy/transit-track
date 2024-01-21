d13eaf35674dc54db794255d4f259b3c1db3facb
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-01-21 00:25:33 -0800

    small frontend changes

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -4,8 +4,8 @@ import { View, Text, StyleSheet } from 'react-native';
 const Profile = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.section}>Name</Text>
-      <Text style={styles.section}>Email</Text>
+      <Text style={styles.section}>*Insert Name Here*</Text>
+      <Text style={styles.section}>* Insert Email Here*</Text>
       <Text style={styles.section}>Item 3</Text>
       <Text style={styles.section}>Item 4</Text>
     </View>
@@ -14,14 +14,22 @@ const Profile = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
+    flex: 1,
     flexDirection: 'column',
-    margin: 4,
+    margin: 5,
+    justifyContent: 'center',
+
+
   },
   section: {
     marginBottom: 4,
     padding: 4,
+    fontSize: 20,
+    fontWeight: 'bold',
+    
   },
+
 });
 
 export default Profile;
+
