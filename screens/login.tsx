2a1cf6402ce737f7304079798dfee675a01d9882
dav <usr21p1@gmail.com>
2024-01-20 22:13:53 -0800

    Settings navigation

--- screens/login.tsx
+++ screens/login.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Button } from "react-native";
+import { SettingsPage } from "./settings";
 
-const Login = () => {
+const Login = ({
+  setSettingsPage,
+}: {
+  setSettingsPage: (next: SettingsPage) => void;
+}) => {
   return (
     <>
-        
+      <Button title="Go Back" onPress={() => setSettingsPage("Settings")} />
     </>
-  )
-}
+  );
+};
 
-export default Login 
+export default Login;
