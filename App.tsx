aae100bf9d14947e9a5e312b0b1e785f46e4c763
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-01-20 20:07:49 -0800

    frontend

--- App.tsx
+++ App.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Navbar from "./components/Navbar";
+import Index from "./Index";
 
 const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Navbar />
+    <View>
+      {/* <Navbar /> */}
     </View>
-    // @ts-ignore - Temporarily ignore the TypeScript error
-    <TailwindProvider utilities={utilities}>
-      <Index />
-    </TailwindProvider>
   );
 };
 
