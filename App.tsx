d705073b769c01817d30f737b5af667b73193cd5
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-01-20 19:17:34 -0800

    frontend - home

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
+import React from 'react'
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Navbar />
     </View>
   );
 }
